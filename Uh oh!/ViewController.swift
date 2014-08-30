fd8eb2c3825fad168a42aa61aaf7d66877e314bb
Benno Ommerborn <Benno@ommerborn.com>
2014-08-30 19:58:30 +0100

    Add location manager and the possibility to call a phone number

--- Uh oh!/ViewController.swift
+++ Uh oh!/ViewController.swift
@@ -8,25 +8,45 @@
 
 import UIKit
 import AVFoundation
+import CoreLocation
 
-class ViewController: UIViewController {
-  @IBOutlet var panicButton: UIButton!
-  @IBOutlet var worryButton: UIButton!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-  @IBAction func MakeCall(sender: AnyObject) {
-    var speaker = AVSpeechSynthesizer()
-    var utterance = AVSpeechUtterance(string: "Look at you hacker, a p-p-pathetic creature of meat and bone, panting and sweating as you run through my corridors.")
-    speaker.speakUtterance(utterance)
-  }
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet var panicButton: UIButton!
+    @IBOutlet var worryButton: UIButton!
+    
+    @IBOutlet weak var curLocation: UILabel!
+    var locationManager:CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func MakeCall(sender: AnyObject) {
+        let phone = "tel://00447477973182";
+        let url:NSURL = NSURL.URLWithString(phone);
+        UIApplication.sharedApplication().openURL(url);
+        
+        
+        var speaker = AVSpeechSynthesizer()
+        var utterance = AVSpeechUtterance(string: "Look at you hacker, a p-p-pathetic creature of meat and bone, panting and sweating as you run through my corridors.")
+        speaker.speakUtterance(utterance)
+    }
+    
+    
+    func locationManager(manager:CLLocationManager, didUpdateLocations locations:[AnyObject]) {
+        println("locations = \(locations)")
+        curLocation.text = "success"
+        
+    }
 }
 
